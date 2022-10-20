0a82b6228ab6becb34fcc56c2d572865797e7c9f
cali1204 <qwertyuiop.s1204@gmail.com>
2022-10-20 00:15:20 -0400

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import NewsItem from './components/NewsItem';
+
 
 export default class App extends Component {
   s = "sukh";
